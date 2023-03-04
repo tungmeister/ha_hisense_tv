181ae89088066bb74645a9d4b3a5b82a93731b1c
Daniel Tye <danieltye@hotmail.com>
2023-03-04 22:52:17 +0000

    Update switch.py
    trying to work out game mode

--- custom_components/hisense_tv/switch.py
+++ custom_components/hisense_tv/switch.py
@@ -36,6 +36,15 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
         ip_address=ip_address,
     )
     async_add_entities([entity])
+    
+        gamemode = HisenseGameModeSwitch(
+        hass=hass,
+        name=name,
+        mqtt_in=mqtt_in,
+        mqtt_out=mqtt_out,
+        uid=uid,
+    )
+    async_add_entities([gamemode])
 
 
 class HisenseTvSwitch(SwitchEntity, HisenseTvBase):
@@ -132,6 +141,106 @@ class HisenseTvSwitch(SwitchEntity, HisenseTvBase):
         self._subscriptions["sourcelist"] = await mqtt.async_subscribe(
             self._hass,
             self._out_topic("/remoteapp/mobile/%s/ui_service/data/sourcelist"),
+            self._message_received_state,
+        )
+
+    async def _message_received_turnoff(self, msg):
+        _LOGGER.debug("message_received_turnoff")
+        self._is_on = False
+        self.async_write_ha_state()
+
+    async def _message_received_state(self, msg):
+        if msg.retain:
+            _LOGGER.debug("SWITCH message_received_state - skip retained message")
+            return
+
+        _LOGGER.debug("SWITCH message_received_state - turn on")
+        self._is_on = True
+        self.async_write_ha_state()
+        
+class HisenseGameModeSwitch(SwitchEntity, HisenseTvBase):
+    """Hisense GameMode switch entity."""
+
+    def __init__(self, hass, name, mqtt_in, mqtt_out, uid):
+        HisenseTvBase.__init__(
+            self=self,
+            hass=hass,
+            name="Projector Game Mode",
+            mqtt_in=mqtt_in,
+            mqtt_out=mqtt_out,
+            uid=uid,
+        )
+        self._is_on = False
+
+    async def async_turn_on(self, **kwargs):
+        """Turn the entity on."""
+        await mqtt.async_publish(
+            hass=self._hass,
+            topic=self._out_topic("/remoteapp/mobile/%s/platform_service/data/picturesetting"),
+            payload='{"action":"resp_set_value","menu_id":122,"menu_value_type":"int", "menu_value":0}',
+            retain=False,
+        )
+
+    async def async_turn_off(self, **kwargs):
+        """Turn the entity off."""
+        await mqtt.async_publish(
+            hass=self._hass,
+            topic=self._out_topic("/remoteapp/mobile/%s/platform_service/data/picturesetting"),
+            payload='{"action":"resp_set_value","menu_id":122,"menu_value_type":"int", "menu_value":0}',
+            retain=False,
+        )
+
+    @property
+    def is_on(self):
+        return self._is_on
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {(DOMAIN, self._unique_id)},
+            "name": self._name,
+            "manufacturer": DEFAULT_NAME,
+        }
+
+    @property
+    def unique_id(self):
+        """Return the unique id of the device."""
+        return self._unique_id
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def icon(self):
+        return "mdi:gamepad-variant"
+
+    @property
+    def device_class(self):
+        _LOGGER.debug("device_class")
+        return DEVICE_CLASS_SWITCH
+
+    @property
+    def should_poll(self):
+        """No polling needed."""
+        return False
+
+    async def async_will_remove_from_hass(self):
+        for unsubscribe in list(self._subscriptions.values()):
+            unsubscribe()
+
+    async def async_added_to_hass(self):
+        self._subscriptions["tvsleep"] = await mqtt.async_subscribe(
+            self._hass,
+            self._in_topic(
+                "/remoteapp/mobile/broadcast/platform_service/actions/tvsleep"
+            ),
+            self._message_received_turnoff,
+        )
+
+        self._subscriptions["state"] = await mqtt.async_subscribe(
+            self._hass,
+            self._in_topic("/remoteapp/mobile/broadcast/ui_service/state"),
             self._message_received_state,
         )
 
